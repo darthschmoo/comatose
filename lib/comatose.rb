4f5e8eceec75baaa25b31b7a5f8e48bda03590df
Polar Humenn <polar@adiron.com>
2010-06-24 21:29:18 -0400

    Requires acts_as_list and acts_as_tree as gems.

--- lib/comatose.rb
+++ lib/comatose.rb
@@ -12,6 +12,8 @@ module Comatose
   
 end
 
+require 'acts_as_list'
+require 'acts_as_tree'
 require 'acts_as_versioned'
 require 'redcloth' unless defined?(RedCloth)
 require 'liquid' unless defined?(Liquid)
